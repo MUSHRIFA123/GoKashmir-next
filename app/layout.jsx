73d64249300f0409c5c1976104020756a96b654e
unknown <mushrifa123@gmail.com>
2026-01-24 15:50:38 +0530

    Added google analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,28 @@
+// // app/layout.js
+// import './globals.css'
+// import Script from "next/script";
+
+// export const metadata = {
+//   title: 'Go Kashmir',
+//   description: 'Your travel guide for Kashmir',
+//   icons: {
+//     icon: '/favicon.ico',
+//   },
+//   verification: {
+//     google: 'KbHGYyqwSZqkddVP5SKR1TO5DgHhvHM1dCk0A2q0WBc',
+//   },
+// }
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   )
+// }
 // app/layout.js
 import './globals.css'
+import Script from "next/script";
 
 export const metadata = {
   title: 'Go Kashmir',
@@ -15,6 +38,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XHTLGGYR9Q"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XHTLGGYR9Q');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
