9777a5cc4414c81c536bbda4f22c4d453d1a1d6d
unknown <mushrifa123@gmail.com>
2026-01-30 16:34:06 +0530

    Updated attractions

--- models/Place.js
+++ models/Place.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 
+// FAQ Schema
 const FAQSchema = new mongoose.Schema({
   question: { type: String, required: true },
   answer: { type: String, required: true },
 });
 
+// Image Schema
+const ImageSchema = new mongoose.Schema({
+  url: { type: String, required: true },
+  alt: { type: String, default: "" }, // optional alt text
+});
+
+// Place Schema
 const PlaceSchema = new mongoose.Schema({
   title: { type: String, required: true },
   slug: { type: String, required: true, unique: true },
@@ -21,8 +29,8 @@ const PlaceSchema = new mongoose.Schema({
   travel_information: { type: String },
   locality: { type: String },
   top: { type: Boolean, default: false },
-  image: { type: String },
+  image: ImageSchema, // store as object with url + alt
   faqs: [FAQSchema],
-});
+}, { timestamps: true });
 
 export default mongoose.models.Place || mongoose.model("Place", PlaceSchema);
