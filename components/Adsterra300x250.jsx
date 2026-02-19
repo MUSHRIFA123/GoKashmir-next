813d8d102a3c711d258223edcc8c30b6895c15d8
unknown <mushrifa123@gmail.com>
2026-02-19 11:07:52 +0530

    Updated

--- components/Adsterra300x250.jsx
+++ components/Adsterra300x250.jsx
@@ -1,31 +1,31 @@
 "use client";
-import { useEffect } from "react";
+
+import Script from "next/script";
 
 export default function Adsterra300x250() {
-  useEffect(() => {
-    const script1 = document.createElement("script");
-    script1.innerHTML = `
-      atOptions = {
-        'key' : 'add9888686e50cfeb824efa1938b6d90',
-        'format' : 'iframe',
-        'height' : 250,
-        'width' : 300,
-        'params' : {}
-      };
-    `;
+  return (
+    <div style={{ width: 300, height: 250 }}>
+      <Script
+        id="adsterra-config"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : 'add9888686e50cfeb824efa1938b6d90',
+              'format' : 'iframe',
+              'height' : 250,
+              'width' : 300,
+              'params' : {}
+            };
+          `,
+        }}
+      />
 
-    const script2 = document.createElement("script");
-    script2.src =
-      "https://www.highperformanceformat.com/add9888686e50cfeb824efa1938b6d90/invoke.js";
-    script2.async = true;
-
-    const container = document.getElementById("adsterra-300");
-
-    if (container && container.childNodes.length === 0) {
-      container.appendChild(script1);
-      container.appendChild(script2);
-    }
-  }, []);
-
-  return <div id="adsterra-300" style={{ minHeight: 250 }} />;
+      <Script
+        id="adsterra-script"
+        strategy="afterInteractive"
+        src="https://www.highperformanceformat.com/add9888686e50cfeb824efa1938b6d90/invoke.js"
+      />
+    </div>
+  );
 }
