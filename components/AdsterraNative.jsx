dc00896ad6c92d50e1fbacf1e1b106def59192a0
unknown <mushrifa123@gmail.com>
2026-02-19 11:26:35 +0530

    Updated

--- components/AdsterraNative.jsx
+++ components/AdsterraNative.jsx
@@ -1,28 +1,31 @@
 "use client";
 
-import { useEffect } from "react";
+import Script from "next/script";
 
-export default function AdsterraNative() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.async = true;
-    script.setAttribute("data-cfasync", "false");
-    script.src =
-      "https://pl28744207.effectivegatecpm.com/53599c170c02b856095533814f017bda/invoke.js";
-
-    const container = document.getElementById(
-      "container-53599c170c02b856095533814f017bda"
-    );
-
-    if (container && !container.hasChildNodes()) {
-      container.appendChild(script);
-    }
-  }, []);
-
+export default function Adsterra300x250() {
   return (
-    <div
-      id="container-53599c170c02b856095533814f017bda"
-      style={{ width: "100%", minHeight: "120px" }}
-    />
+    <div style={{ width: 300, height: 250 }}>
+      <Script
+        id="adsterra-config"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : 'add9888686e50cfeb824efa1938b6d90',
+              'format' : 'iframe',
+              'height' : 250,
+              'width' : 300,
+              'params' : {}
+            };
+          `,
+        }}
+      />
+
+      <Script
+        id="adsterra-script"
+        strategy="afterInteractive"
+        src="https://www.highperformanceformat.com/add9888686e50cfeb824efa1938b6d90/invoke.js"
+      />
+    </div>
   );
 }
