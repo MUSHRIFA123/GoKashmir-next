0313997004e1a75940a5493d9ba34a3aae222799
unknown <mushrifa123@gmail.com>
2026-02-19 10:35:42 +0530

    Updated layout page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,42 @@
+// import './globals.css'
+// import Script from "next/script";
+
+// export const metadata = {
+//   title: 'Go Kashmir',
+//   description: 'Your travel guide for Kashmir',
+//   icons: {
+//     icon: '/favicon.ico',
+//   },
+//   verification: {
+//     google: 'KbHGYyqwSZqkddVP5SKR1TO5DgHhvHM1dCk0A2q0WBc',
+//   },
+// }
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         {/* Adsense */}
+//         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7496144172676724"
+//      crossorigin="anonymous"></script>
+//         {/* Google Analytics */}
+//         <Script
+//           src="https://www.googletagmanager.com/gtag/js?id=G-XHTLGGYR9Q"
+//           strategy="afterInteractive"
+//         />
+//         <Script id="google-analytics" strategy="afterInteractive">
+//           {`
+//             window.dataLayer = window.dataLayer || [];
+//             function gtag(){dataLayer.push(arguments);}
+//             gtag('js', new Date());
+//             gtag('config', 'G-XHTLGGYR9Q');
+//           `}
+//         </Script>
+//       </head>
+//       <body>{children}</body>
+//     </html>
+//   )
+// }
 import './globals.css'
 import Script from "next/script";
 
@@ -15,15 +54,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Adsense */}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7496144172676724"
-     crossorigin="anonymous"></script>
+      <head />
+
+      <body>
+
+        {/* ✅ AdSense verification script */}
+        <Script
+          id="adsense-script"
+          strategy="beforeInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7496144172676724"
+          crossOrigin="anonymous"
+        />
+
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-XHTLGGYR9Q"
           strategy="afterInteractive"
         />
+
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -32,8 +80,10 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-XHTLGGYR9Q');
           `}
         </Script>
-      </head>
-      <body>{children}</body>
+
+        {children}
+
+      </body>
     </html>
   )
 }
